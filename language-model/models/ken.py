1530de87e32541e1d4df6cc01fafc56b027ce584
Caitlin Chiang <chiangcaitlin2003@gmail.com>
2024-10-09 11:04:21 +0800

    Fix: import

--- language-model/models/ken.py
+++ language-model/models/ken.py
@@ -1,8 +1,12 @@
 # IMPORTS
+import sys
+import os
 import kenlm
-from ...data.hypotheses_samples.hypotheses import hypotheses
 
-model = kenlm.Model('/Users/caitlin/_Caitlin/Code/RobustGER-Experimentation/language-model/tatoeba_3gram.bin')
+sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), '../../')))
+from data.hypotheses_samples.hypotheses import hypotheses
+
+model = kenlm.Model('/Users/caitlin/_Caitlin/Code/RobustGER-Experimentation/language-model/train_kenlm/output/tatoeba_3gram.bin')
 
 scores = [(hypothesis, model.score(hypothesis)) for hypothesis in hypotheses]
 best_hypothesis = sorted(scores, key=lambda x: x[1], reverse=True)[0]
